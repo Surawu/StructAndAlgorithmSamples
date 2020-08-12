7e2a3e8916985777564638bdd5370aa896006a62
wuzhaoyu <18261446150@163.com>
2020-08-13 07:14:23 +0800

    feat: add link list

--- LinkedList/InkList.h
+++ LinkedList/InkList.h
@@ -1,59 +1,118 @@
+#include "Link.h"
+#include <iostream>
 
-template <typename T> class InkList :public list<T>
+template <typename T> class InkList
 {
 private:
 	Link<T>* head, * tail;
-	Link<T>* setPos(const int i)
+	int curLength;
+	Link<T>* setPos(const int i) // 时间复杂度 O(n)
 	{
-		int count=0;
-		if (i==-1)
+		int count = 0;
+		if (i == -1)
 		{
 			return head;
 		}
-		
-		Link<T> *p=new Link<T>(head->next);
-		while (p!=NULL && count <i)
+
+		Link<T>* p = new Link<T>(head->next);
+		while (p != nullptr && count < i)
 		{
-			p=p->next;
+			p = p->next;
 			count++;
 		}
 		return p;
 	}
 
 public:
-	InkList(int s);
+	InkList(const int s)
+	{
+		head = tail = new Link<T>(nullptr);
+	}
+	~InkList()
+	{
+		Link<T>* tmp;
+		while (head != nullptr)
+		{
+			tmp = head;
+			head = head->next;
+			delete tmp;
+		}
+	}
 
-	~InkList();
-	bool IsEmpty();
-	void Clear();
-	int Length();
-	bool Append(T value);
-	bool Insert(int p, T value);
-	bool Delete(int p);
+	bool IsEmpty()
+	{
+		if (head->next == nullptr)
+		{
+			return true;
+		}
+
+		return false;
+	}
+
+	void Clear()
+	{
+		head = tail = new Link<T>;
+	}
+
+	int Length()
+	{
+		return curLength;
+	}
+
+	bool Append(T value)
+	{
+		Link<T>* q = new Link<T>(value, tail->next);
+		tail->next = q;
+		tail = q;
+
+		curLength++;
+		return true;
+	}
+
+	bool Insert(int i, T value)
+	{
+		Link<T>* p, * q;
+		if ((p = setPos(i - 1)) == NULL)
+		{
+			std::cout << "非法插入地點" << std::endl;
+		}
+		q = new Link<T>(value, p->next);
+		p->next = q;
+		if (p == tail)
+		{
+			tail = q;
+		}
+
+		curLength++;
+		return true;
+	}
+
+	bool Delete(int i)
+	{
+		Link<T>* p, * q;
+		if ((p = setPos(i - 1)) == nullptr || p == tail)
+		{
+			std::cout << "invalid delete point" << std::endl;
+			return false;
+		}
+
+		q = p->next;
+		if (q == tail)
+		{
+			tail = p;
+			p->next = nullptr;
+			delete q;
+		}
+		else if (q != nullptr)
+		{
+			p->next = q->next;
+			delete q;
+		}
+		curLength--;
+		return true;
+	}
+
 	bool GetValue(int p, T& value);
 	bool GetPos(int& p, T value);
 };
 
-template<typename T>
-inline InkList<T>::InkList(int s)
-{
-	head = tail = new Link<T>();
-}
-
-template<typename T>
-inline InkList<T>::~InkList()
-{
-	Link<T>* tmp;
-	while (head != NULL)
-	{
-		tmp = head;
-		head = head->next;
-		delete tmp;
-	}
-}
-
-template<typename T>
-inline bool InkList<T>::IsEmpty()
-{
-	return false;
-}

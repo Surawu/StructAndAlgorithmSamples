ccb180ca2faf6c9cc02fb621e2de2220eb95aba1
wuzhaoyu <18261446150@163.com>
2020-10-11 15:33:34 +0800

    feat: add two project. 有效的括号, 简化路径

---
+++ Trap/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplifyPath
+{
+    /// <summary>
+    /// 简化路径
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Method("/../"));
+        }
+
+        public static string Method(string path)
+        {
+            var subs = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+            if (subs.Length == 0)
+            {
+                return "/";
+            }
+
+            var stack = new Stack<string>();
+            foreach (var item in subs)
+            {
+                if (item.Equals(".."))
+                {
+                    if (stack.Count != 0)
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    if (!item.Equals("."))
+                    {
+                        stack.Push(item);
+                    }
+                }
+            }
+
+            // 注意点
+            if (stack.Count == 0)
+            {
+                return "/";
+            }
+
+            var sb = new StringBuilder();
+            var arr = stack.ToArray();
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                sb.Append($"/{arr[i]}");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ValidBracket/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ValidBracket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var re = Method("12");
+            Console.WriteLine(re);
+        }
+
+        public static bool Method(string s)
+        {
+            var dics = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            var chars = s.ToCharArray();
+            if (chars.Length == 0)
+            {
+                return true;
+            }
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (var item in chars)
+            {
+                if (dics.ContainsKey(item))
+                {
+                    stack.Push(item);
+                }
+                else
+                {
+                    // 注意：判断栈是否有元素
+                    if (stack.Count == 0 || dics[stack.Pop()] != item)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            // 注意：栈中不能含有元素
+            return stack.Count == 0;
+        }
+    }
+}
